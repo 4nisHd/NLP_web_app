8a4b1633e18ac6ba8f1385d4dc6a229db7e3c9d3
Anisx-D <anis.benhouidi@ensi-uma.tn>
2024-04-21 21:59:40 +0100

    increment 3

--- frontend/src/views/Index.js
+++ frontend/src/views/Index.js
@@ -3,37 +3,55 @@ import Typewriter from 'components/Typewriter';
 import { useState } from 'react';
 import axios from 'axios';
 import Wordwriter from 'components/Wordwriter';
+
 const Index = () => {
   const [paragraph, setParagraph] = useState('');
+  const [textData, setTextData] = useState(''); // State to store text data from the textarea
 
   const handleClick = async (button) => {
     try {
-      const response = await axios.post('http://127.0.0.1:5000/api/get-paragraph', { buttonClick: button });
+      const response = await axios.post('http://127.0.0.1:5000/api/get-paragraph', { buttonClick: button, textData });
       setParagraph(response.data.paragraph);
     } catch (error) {
       console.error('Error:', error);
     }
   };
 
+  const handleTextareaChange = (event) => {
+    setTextData(event.target.value); // Update the text data state as the user types in the textarea
+  };
+
   return (
     <div className='bg-light'>
       <div className='bg-warning mx-auto pl-9'>
-          <h1 className='font-weight-bold'>NLP.AI: <Typewriter text="Revolutionizing NLP with Transformers" delay={200} infinite />
-</h1>
-</div>
-<form>
-
-<div class="form-group w-50 mx-auto pt-9 text-center ">
-<h1 className='text-center mb-5'>Text Summerization</h1>
-
-    <textarea placeholder='Enter text...' class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-
-  </div>
-  <div class="col-md-12 text-center">
-            <button type="button" class="btn btn-warning" onClick={() => handleClick('button1')}>Summarize</button>
-            <p><Wordwriter text={paragraph} delay={150} /></p>
-        </div></form>
-</div>
+        <h1 className='font-weight-bold'>
+          NLP.AI: <Typewriter text="Revolutionizing NLP with Transformers" delay={200} infinite />
+        </h1>
+      </div>
+      <form>
+        <div className="form-group w-50 mx-auto pt-9 text-center">
+          <h1 className='text-center mb-5'>Text Summarization</h1>
+          <textarea
+            placeholder='Enter text...'
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="6"
+            value={textData} // Bind the value of the textarea to the textData state
+            onChange={handleTextareaChange} // Call handleTextareaChange on textarea change
+          ></textarea>
+        </div>
+        <div className="col-md-12 text-center">
+          <button
+            type="button"
+            className="btn btn-warning"
+            onClick={() => handleClick('button1')}
+          >
+            Summarize
+          </button>
+          <p className=' pt-7 text-left font-weight-bold'><Wordwriter text={paragraph} delay={150} /></p>
+        </div>
+      </form>
+    </div>
   );
 };
 
